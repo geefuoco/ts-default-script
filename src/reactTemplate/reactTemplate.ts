644dd71f9c2e9d3e7240fdbafa52b8aa49b63470
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-03-09 18:20:42 -0500

    Created a function to create a template for react including the components folder, App.tsx

---
+++ src/reactTemplate/reactTemplate.ts
@@ -0,0 +1,49 @@
+import { generateHtmlTemplate } from "../htmlTemplate/htmlTemplate";
+
+interface ReactFiles {
+  index: string;
+  app: string;
+  html: string;
+}
+
+function createReactTemplate(projectName = "Create Ts Project React"): string {
+  let html = generateHtmlTemplate(projectName);
+  const reactHtml = `
+<noscript>Please enable javascript to use this app. </noscript/>
+<div id="root"></div>
+  `;
+  html = html.replace(/<!-- template -->/, reactHtml);
+  return html;
+}
+
+function generateReactFiles(projectName: string): ReactFiles {
+  const index = `
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+  `;
+
+  const html = createReactTemplate(projectName);
+
+  const app = `
+import React from "react";
+
+const App:React.FC = () => {
+  return <div className="app">Hello, World!</div>;
+}
+
+export default App;
+  `;
+
+  return { index, app, html };
+}
+
+export { generateReactFiles };
