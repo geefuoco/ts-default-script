18f2db04f8a8100db7dae00289243a852d0e4a12
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-06-24 21:01:15 -0400

    updated react template for react 18

--- src/reactTemplate/reactTemplate.ts
+++ src/reactTemplate/reactTemplate.ts
@@ -19,15 +19,16 @@ function createReactTemplate(projectName = "Create Ts Project React"): string {
 function generateReactFiles(projectName: string): ReactFiles {
   const index = `
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(
+root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
   `;
 
