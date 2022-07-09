21fb54ad3387ce601c39d4ea143ae24e86936acf
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-07-09 00:13:19 -0400

    updated tsconfig to include esModuleInterop

--- src/configfiles/typescriptConfigGenerator.ts
+++ src/configfiles/typescriptConfigGenerator.ts
@@ -1,22 +1,23 @@
 export function createTypescriptConfig(projectType: string) {
-  const config = {
-    compilerOptions: {
-      rootDir: "src",
-      outDir: "build",
-      target: "es6",
-      allowJs: true,
-      moduleResolution: "node",
-      module: projectType === "Webpack" ? "es6" : "commonjs",
-      sourceMap: true,
-      allowSyntheticDefaultImports: true,
-      forceConsistentCasingInFileNames: true,
-      jsx: "react",
-      strict: true,
-      skipLibCheck: true
-    },
-    include: ["src/**/*"],
-    exclude: ["node_modules", "build"]
-  };
+    const config = {
+        compilerOptions: {
+            rootDir: "src",
+            outDir: "build",
+            target: "es6",
+            allowJs: true,
+            moduleResolution: "node",
+            esModuleInterop: true,
+            module: projectType === "Webpack" ? "es6" : "commonjs",
+            sourceMap: true,
+            allowSyntheticDefaultImports: true,
+            forceConsistentCasingInFileNames: true,
+            jsx: "react",
+            strict: true,
+            skipLibCheck: true
+        },
+        include: ["src/**/*"],
+        exclude: ["node_modules", "build"]
+    };
 
-  return JSON.stringify(config, null, " ");
+    return JSON.stringify(config, null, " ");
 }
